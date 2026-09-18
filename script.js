fb07cc5bb8703a5c7f7b280f355e3ab8568f96ef
bryambarbacovi-hub <bryam.barbacovi@escola.pr.gov.br>
2026-09-18 08:50:28 -0300

    salvei

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const botoesCurtir = document.querySelectorA11("curtir");
+botaocurtir.array.forEach(function(botaoCurtir){
+let curtiu = false;
+botaoCurtir.addEventlistener('click', curtir);
+function curtir(){
+const contador = botao.querySelector("span");
+if(curtiu===false){
+contador.textcontent++;
+curtiu = true;}
+ else{
+    contador.textcontent--;
+    curtiu = false;
+ }
 
+}
+});
